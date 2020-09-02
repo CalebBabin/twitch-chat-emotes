f0af629335ee739878a5c14f30f6337b8d212d75
Caleb <CalebBabin@users.noreply.github.com>
2020-09-02 09:44:01 -0500

    account for delay multiplier

--- gifLoader.js
+++ gifLoader.js
@@ -32,7 +32,7 @@ class GIF_Instance {
 						for (let index = 0; index < this.frames.length; index++) {
 							const frame = this.frames[index];
 
-							if (frame.delay < 1) frame.delay = 1000 / 60;
+							if (frame.delay < 1) frame.delay = 1000 / 60 / 10;
 
 							frame.image = new Image(frame.width, frame.height);
 							frame.image.crossOrigin = "";
